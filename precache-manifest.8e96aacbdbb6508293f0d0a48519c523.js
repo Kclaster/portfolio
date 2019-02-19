fc12bb81341a776a7c9717b39a76c8156e8768a8
Kyle Laster <kyle.laster@gmail.com>
2019-02-19 12:11:12 -0600

    Updates

---
+++ precache-manifest.8e96aacbdbb6508293f0d0a48519c523.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7166aac2db4c173c3b9ab720ac773f90",
+    "url": "/portfolio/static/media/fight.7166aac2.png"
+  },
+  {
+    "revision": "d6c3d7b211ef2482d2dc",
+    "url": "/portfolio/static/css/main.f22e0019.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "d93aad53602fd7128c12",
+    "url": "/portfolio/static/js/2.d93aad53.chunk.js"
+  },
+  {
+    "revision": "6a804d2802eaf5871fc9664e84261b83",
+    "url": "/portfolio/static/media/background.6a804d28.png"
+  },
+  {
+    "revision": "54d6ebba6ca91c350616b09ebb998a8a",
+    "url": "/portfolio/static/media/gif.54d6ebba.png"
+  },
+  {
+    "revision": "39d063173ab55bad794594228e108a4d",
+    "url": "/portfolio/static/media/trivia.39d06317.png"
+  },
+  {
+    "revision": "d6c3d7b211ef2482d2dc",
+    "url": "/portfolio/static/js/main.d6c3d7b2.chunk.js"
+  },
+  {
+    "revision": "f524838430a69957ab856adfb639ae6b",
+    "url": "/portfolio/static/media/word.f5248384.png"
+  },
+  {
+    "revision": "97cbe4d4040fd80127e266332feace5e",
+    "url": "/portfolio/static/media/cinimania.97cbe4d4.png"
+  },
+  {
+    "revision": "c40140880c6a655ace54b053a3db5feb",
+    "url": "/portfolio/static/media/desk.c4014088.jpg"
+  },
+  {
+    "revision": "a71142b74accfa0ea317be4432472a36",
+    "url": "/portfolio/static/media/vertical2.a71142b7.jpg"
+  },
+  {
+    "revision": "0c7f7e09bc5707548428d206711811c4",
+    "url": "/portfolio/static/media/myface.0c7f7e09.jpg"
+  },
+  {
+    "revision": "2904c0dc005d8388a4d7bb5f278aa644",
+    "url": "/portfolio/static/media/my-resume.2904c0dc.pdf"
+  },
+  {
+    "revision": "77ab0841cf8ae1aa72146af796d7ea88",
+    "url": "/portfolio/index.html"
+  }
+];
